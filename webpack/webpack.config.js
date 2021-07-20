7126a3967c3bf85dbac8ef437371cc1f76d284f3
stokes-christopher <christopher.stokes@symbl.ai>
2021-07-20 15:03:34 -0500

    update tsc build directory name

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -10,7 +10,7 @@ var nodeConfig = {
         'websocket': 'websocket',
         'formidable': 'formidable'
     },
-    entry: './dist/index.js',
+    entry: './build-tsc/index.js',
     resolve: {
         modules: ['node_modules/@rammerai/api-client/src', 'node_modules']
     },
@@ -40,7 +40,7 @@ var nodeConfig = {
 
 var browserConfig = {
     target: 'web',
-    entry: './dist/index.js',
+    entry: './build-tsc/index.js',
     resolve: {
         modules: ['node_modules/@rammerai/api-client/src', 'node_modules']
     },
