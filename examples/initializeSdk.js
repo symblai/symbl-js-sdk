3a651c6bbf248bbd69bf545690e72820006cbd15
stokes-christopher <christopher.stokes@symbl.ai>
2021-07-20 15:03:47 -0500

    update initialize example

--- examples/initializeSdk.js
+++ examples/initializeSdk.js
@@ -1,7 +1,10 @@
 const {sdk} = require('../build/app.bundle');
 
+const APP_ID = "<insert App ID";
+const APP_SECRET = "<insert App Secret>";
+
 sdk.init({
-    appId: 'yourAppId',
-    appSecret: 'yourAppSecret'
+    appId: APP_ID,
+    appSecret: APP_SECRET
 }).then(() => console.log('SDK Initialized.'))
     .catch(err => console.error('Error in initialization.', err));
