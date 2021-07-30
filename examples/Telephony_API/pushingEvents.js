69febd77d3c8bb24c2e0f19a6031e92a1b10e5ae
stokes-christopher <christopher.stokes@symbl.ai>
2021-07-29 22:16:09 -0500

    add events example to Telephony API examples

---
+++ examples/Telephony_API/pushingEvents.js
@@ -0,0 +1,53 @@
+const { sdk, SpeakerEvent } = require('symbl-node');
+
+const APP_ID = '<your App ID>';
+const APP_SECRET = '<your App Secret>';
+const PHONE_NUMBER = '<your phone number>';
+
+(async () => {
+  try {
+    await sdk.init({
+      appId: APP_ID,
+      appSecret: APP_SECRET
+    })
+    const connection = await sdk.startEndpoint({
+        endpoint: {
+            type: 'pstn',
+            phoneNumber: PHONE_NUMBER
+        }
+    })
+    
+    const { connectionId } = connection;
+    console.log(`Successfully connected. ConnectionId: ${connectionId}`);
+
+    const speakerEvent = new SpeakerEvent();
+    speakerEvent.type = SpeakerEvent.types.startedSpeaking;
+    speakerEvent.user = {
+        userId: 'john@example.com',
+        name: 'John'
+    };
+    speakerEvent.timestamp = new Date().toISOString();
+
+    // Use pushEventOnConnection from the sdk instance to push event on connection
+    sdk.pushEventOnConnection(connectionId, speakerEvent.toJSON(), (err) => {
+        if (err) {
+            console.error('Error during push event.', err);
+        } else {
+            console.log('Event pushed!');
+        }
+    });
+
+    // Scheduling stop endpoint call after 60 seconds for the demonstration purpose
+    // In real adoption, sdk.stopEndpoint() should be called when the meeting or call actually ends
+    setTimeout(() => {
+        sdk.stopEndpoint({
+            connectionId: connection.connectionId
+        }).then(() => {
+            console.log('Stopped the connection');
+        }).catch(err => console.error('Error while stopping the connection.', err));
+    }, 60 * 1000);
+  
+  } catch (err) {
+    console.error('Error in SDK initialization.', err);
+  }
+})
