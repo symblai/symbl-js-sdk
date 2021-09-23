b24841d6ce36ee43546a05b0fc2e1f7e16747ec4
Christopher Stokes <christopher.stokes@symbl.ai>
2021-09-23 18:05:49 -0500

    Refactor: remove moment package

--- src/event/SpeakerEvent.js
+++ src/event/SpeakerEvent.js
@@ -1,74 +1,99 @@
-import moment from 'moment';
-
 export default class SpeakerEvent {
 
-    static get types() {
-        return {
-            startedSpeaking: 'started_speaking',
-            stoppedSpeaking: 'stopped_speaking',
-            joined: 'joined',
-            left: 'left'
-        }
-    };
+    static get types () {
 
-    constructor(options = {}) {
+        return {
+            "startedSpeaking": "started_speaking",
+            "stoppedSpeaking": "stopped_speaking",
+            "joined": "joined",
+            "left": "left"
+        };
+
+    }
+
+    constructor (options = {}) {
+
         if (!options) {
-            throw new Error('SpeakerEvent configuration is required.');
+
+            throw new Error("SpeakerEvent configuration is required.");
+
         }
 
         if (!options.type) {
-            throw new Error(`'type' is required parameter for speaker event`);
+
+            throw new Error("'type' is required parameter for speaker event");
+
         }
 
         if (!options.user) {
-            throw new Error(`'user' is required parameter for speaker event`);
+
+            throw new Error("'user' is required parameter for speaker event");
+
         }
 
         if (!options.user.userId) {
-            throw new Error(`'userId' is required parameter in 'user' for speaker event`);
+
+            throw new Error("'userId' is required parameter in 'user' for speaker event");
+
         }
 
-        this.topic = 'speaker';
+        this.topic = "speaker";
         this._type = options.type;
         this._user = options.user;
-        this._timestamp = options.timestamp ?
-            moment(options.timestamp).toISOString() :
-            options._timestamp ?
-                moment(options._timestamp).toISOString() :
-                moment().toISOString();
+        this._timestamp = options.timestamp
+            ? new Date(options.timestamp).toISOString()
+            : options._timestamp
+                ? new Date(options._timestamp).toISOString()
+                : new Date().toISOString();
+
     }
 
 
-    get type() {
+    get type () {
+
         return this._type;
+
     }
 
-    set type(value) {
+    set type (value) {
+
         this._type = value;
+
     }
 
-    get user() {
+    get user () {
+
         return this._user;
+
     }
 
-    set user(value) {
+    set user (value) {
+
         this._user = value;
+
     }
 
-    get timestamp() {
+    get timestamp () {
+
         return this._timestamp;
+
     }
 
-    set timestamp(value) {
+    set timestamp (value) {
+
         this._timestamp = value;
+
     }
 
-    toJSON() {
+    toJSON () {
+
         return {
-            topic: this.topic,
-            type: this._type,
-            user: this._user,
-            timestamp: this._timestamp
-        }
+            "topic": this.topic,
+            "type": this._type,
+            "user": this._user,
+            "timestamp": this._timestamp
+        };
+
     }
+
 }
