54cf2fc9afc950405e019b7ce8b7fe6357901076
avoliva <adam.voliva@symbl.ai>
2021-11-09 20:56:33 -0800

    Use web sdk in react app.

--- examples/closed-captions-browser/src/App.js
+++ examples/closed-captions-browser/src/App.js
@@ -24,6 +24,8 @@ import { green } from '@mui/material/colors';
 
 import { sdk } from "@symblai/symbl-js/build/client.sdk.min.js";
 
+import symbl from "@symblai/symbl-web-sdk";
+
 import { v4 } from "uuid";
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -56,42 +58,40 @@ const handleMicEvent = (setMuted, setMuting, setCaption) => {
             setMuting(true);
 
             if (muted) {
-                context && context.resume();
-                if (stream) {
-                    await stream.start(
-                        context && context.sampleRate
-                            ? {
-                                  config: {
-                                      sampleRateHertz: context.sampleRate,
-                                  },
-                              }
-                            : {}
-                    );
-                    window.connectionActive = true;
-                    setMuted(false);
+                symbl.unmute(stream);
+                setMuted(false);
+                // context && context.resume();
+                // if (stream) {
+                //     await stream.start(
+                //         context && context.sampleRate
+                //             ? {
+                //                   config: {
+                //                       sampleRateHertz: context.sampleRate,
+                //                   },
+                //               }
+                //             : {}
+                //     );
+                window.connectionActive = true;
+                //     setMuted(false);
 
-                    gainNode.gain.value = 1;
-                    setCaption(
-                        "You're unmuted. Live captions will appear here..."
-                    );
-                }
+                //     gainNode.gain.value = 1;
+                setCaption(
+                    "You're unmuted. Live captions will appear here..."
+                );
+                // }
             } else {
-                context && context.suspend();
-                if (stream) {
-                    gainNode.gain.value = 0;
-                    await stream.stop();
+                
+                symbl.mute(stream);
+                window.connectionActive = false;
 
-                    window.connectionActive = false;
-
-                    setMuted(true);
-                    setTimeout(() => {
-                        if (!window.connectionActive) {
-                            setCaption(
-                                "You're muted. Click the unmute button to resume captions."
-                            );
-                        }
-                    }, 3000);
-                }
+                setMuted(true);
+                setTimeout(() => {
+                    if (!window.connectionActive) {
+                        setCaption(
+                            "You're muted. Click the unmute button to resume captions."
+                        );
+                    }
+                }, 3000);
             }
 
             setMuting(false);
@@ -102,125 +102,20 @@ const handleMicEvent = (setMuted, setMuting, setCaption) => {
 };
 
 const initSDK = async () => {
-    await sdk.init({
+    await symbl.init({
         appId: process.env.REACT_APP_APP_ID,
         appSecret: process.env.REACT_APP_APP_SECRET,
         basePath: process.env.REACT_APP_BASE_PATH,
     });
 };
 
-const isAppleMicrophone = (device) => {
-    return (
-        device.label &&
-        (device.label.includes("MacBook") ||
-            device.label.includes("iPhone") ||
-            device.label.includes("iPad"))
-    );
-};
-
-const getUserMediaStream = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    console.log("All Devices: ", devices);
-    let device = devices.filter((device) => isAppleMicrophone(device));
-    if (device.length > 0 && window.safari !== undefined) {
-        console.log("Detected Safari. Using device: ", device[0]);
-        return navigator.mediaDevices.getUserMedia({
-            audio: {
-                deviceId: device[0].deviceId,
-            },
-            video: false,
-        });
-    } else {
-        try {
-            console.log("Not safari");
-            const defaultDevice = devices.filter(
-                (device) =>
-                    device.deviceId === "default" &&
-                    device.kind === "audioinput"
-            );
-            console.log("Default device: ", defaultDevice);
-            if (defaultDevice.length > 0) {
-                const device = devices.filter(
-                    (device) =>
-                        device.deviceId !== "default" &&
-                        defaultDevice[0].label.includes(device.label) &&
-                        device.kind === "audioinput"
-                );
-
-                console.log("Default device matches: ", device);
-
-                if (device.length > 0) {
-                    console.log(
-                        "The device to be used for getting stream: ",
-                        device
-                    );
-                    return navigator.mediaDevices.getUserMedia({
-                        audio: {
-                            deviceId: device[0].deviceId,
-                        },
-                        video: false,
-                    });
-                } else {
-                    return navigator.mediaDevices.getUserMedia({
-                        audio: true,
-                        video: false,
-                    });
-                }
-            } else {
-                return navigator.mediaDevices.getUserMedia({
-                    audio: true,
-                    video: false,
-                });
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    }
-};
-
-const processAudio = async (e) => {
-    if (window.connectionActive) {
-        const inputData =
-            e.inputBuffer.getChannelData(0) ||
-            new Float32Array(this.options.bufferSize);
-
-        // console.log(e.inputBuffer);
-
-        const targetBuffer = new Int16Array(inputData.length);
-        for (let index = inputData.length; index > 0; index--)
-            targetBuffer[index] = 32767 * Math.min(1, inputData[index]);
-
-        stream && stream.sendAudio(targetBuffer);
-    }
-};
-
-const startAudio = async (muted, muting) => {
-    if (!context) {
-        context = new AudioContext();
-
-        const audioStream = await getUserMediaStream();
-        processor = context.createScriptProcessor(2048, 1, 1);
-
-        source = context.createMediaStreamSource(audioStream);
-
-        gainNode = context.createGain();
-        gainNode.gain.value = 0;
-
-        source.connect(gainNode);
-        gainNode.connect(processor);
-        processor.connect(context.destination);
-
-        processor.onaudioprocess = processAudio;
-    }
-};
-
-const initiateConnection = async (handlers, sampleRateHertz) => {
+const initiateConnection = async (handlers) => {
     const id = v4();
     console.log(id);
 
     connectionId = id;
 
-    stream = await sdk.createStream({
+    stream = await symbl.startRealtimeRequest({
         id,
         disconnectOnStopRequest: false,
         disconnectOnStopRequestTimeout: 300,
@@ -231,14 +126,13 @@ const initiateConnection = async (handlers, sampleRateHertz) => {
             confidenceThreshold: 0.7,
             timezoneOffset: 480, // Offset in minutes from UTC
             languageCode: "en-US",
-            sampleRateHertz,
         },
         speaker: {
             userId: process.env.USER_ID || "tanaka@example.com", // Update with valid email or a unique user id
             name: process.env.FULL_NAME || "Tanaka",
         },
         handlers,
-    });
+    }, true);
 
     return stream;
 };
@@ -332,7 +226,6 @@ function App() {
             setCaption("Initalizing connection...");
 
             if (!started) {
-                await startAudio(muted, muting);
 
                 await initiateConnection(
                     {
@@ -347,8 +240,7 @@ function App() {
                         onStartedListening: () => {
                             setMuted(false);
                         },
-                    },
-                    context && context.sampleRate
+                    }
                 );
 
                 setCaption(
@@ -357,15 +249,15 @@ function App() {
                 setStarted(true);
             } else {
                 if (stream) {
-                    context.close();
+                    // context.close();
 
-                    context = null;
-                    gainNode = null;
-                    processor = null;
+                    // context = null;
+                    // gainNode = null;
+                    // processor = null;
 
                     await stream.close();
 
-                    stream = null;
+                    // stream = null;
                 }
 
                 setStarted(false);
